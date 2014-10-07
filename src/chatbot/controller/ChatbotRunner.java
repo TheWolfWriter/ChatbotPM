a7ddf93805443a3a79a1a585d1492b0c0046789a
S. McKell Nichols <mckellnichols@hotmail.com>
2014-10-07 13:36:18 -0600

    Added documentation to runner.

--- src/chatbot/controller/ChatbotRunner.java
+++ src/chatbot/controller/ChatbotRunner.java
@@ -1,7 +1,17 @@
 package chatbot.controller;
 
+/**
+ * The runner for the Chatbot.
+ * @author S. McKell Nichols
+ * @version 10/1/14
+ */
+
 public class ChatbotRunner
 {
+	/**
+	 * Staring point for all Java programs.
+	 * @param args Unused!
+	 */
 	public static void main(String [] args)
 	{
 		ChatbotAppController appController = new ChatbotAppController();
