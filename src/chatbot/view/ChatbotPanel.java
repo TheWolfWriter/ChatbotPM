4e583186ba8323ae56d21677b3983a13c23d19f7
S. McKell Nichols <mckellnichols@hotmail.com>
2014-10-21 13:44:05 -0600

    Made firstButton do stuff and added chatArea and chatPane

--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -1,9 +1,13 @@
 package chatbot.view;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
@@ -22,6 +26,8 @@ public class ChatbotPanel extends JPanel
 	private JButton firstButton;
 	private JTextField firstTextField;
 	private SpringLayout baseLayout;
+	private JTextArea chatArea;
+	private JScrollPane chatPane;
 	
 	public ChatbotPanel(ChatbotAppController baseController) 
 	{
@@ -30,10 +36,18 @@ public class ChatbotPanel extends JPanel
 		firstButton = new JButton("click the button... it is so clicky :D");
 		firstTextField = new JTextField(25);
 		baseLayout = new SpringLayout();
+		chatPane = new JScrollPane();
 		
+		setupPane();
 		setupPanel();
 		setupLayout();
 		setupListeners();
+	}
+	
+	private void setupPane()
+	{
+		chatArea.setLineWrap(true);;
+		chatArea.setWrapStyleWord(true);
 	}
 	
 	private void setupPanel()
@@ -43,6 +57,11 @@ public class ChatbotPanel extends JPanel
 		this.setSize(400, 400);
 		this.add(firstButton);
 		this.add(firstTextField);
+		this.add(chatPane);
+		chatArea = new JTextArea(5,20);
+		baseLayout.putConstraint(SpringLayout.NORTH, chatArea, 120, SpringLayout.SOUTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.WEST, chatArea, 117, SpringLayout.WEST, this);
+		add(chatArea);
 	}
 	
 	private void setupLayout()
@@ -56,7 +75,13 @@ public class ChatbotPanel extends JPanel
 	
 	private void setupListeners()
 	{
-		
+		firstButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				firstTextField.setText(firstTextField.getText()+ " >:D ");
+			}
+		});
 	}
 
 }
